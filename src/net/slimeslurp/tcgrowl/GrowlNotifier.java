6c19fa1a792d55a2b32f121d6e887ca20c9367f8
Nate Drake <ndrake@gmail.com>
2008-04-09 02:00:16 +0000

    Get user settings the right way.  Thanks, Pavel! Some code clean up
    git-svn-id: http://tcgrowl.googlecode.com/svn/trunk@10 92fe3055-194a-0410-8488-2d504122b2f1

--- src/net/slimeslurp/tcgrowl/GrowlNotifier.java
+++ src/net/slimeslurp/tcgrowl/GrowlNotifier.java
@@ -12,6 +12,7 @@ import jetbrains.buildServer.notification.Notificator;
 import jetbrains.buildServer.serverSide.SBuildType;
 import jetbrains.buildServer.serverSide.SRunningBuild;
 import jetbrains.buildServer.serverSide.UserPropertyInfo;
+import jetbrains.buildServer.users.NotificatorPropertyKey;
 import jetbrains.buildServer.users.PropertyKey;
 import jetbrains.buildServer.users.SUser;
 
@@ -33,16 +34,17 @@ public class GrowlNotifier implements Notificator {
 
     private static final Logger LOG = Logger.getInstance(GrowlNotifier.class.getName());
     
+    /** Holds the user configuration properties */
     private ArrayList<UserPropertyInfo> userProps;
     
+    private static final String TYPE = "tcGrowlNotifier";
     private static final String GROWL_SERVER_KEY = "tcgrowl.gServer";
     private static final String GROWL_PASSWORD_KEY = "tcgrowl.gPassword";
-    
-    private static final String PROP_ROOT = "plugin:notificator:tcGrowlNotifier:";
-    
     private static final String APP_NAME = "Team City";
-    Growl growl;
-    
+        
+    private static final PropertyKey SERVER_KEY = new NotificatorPropertyKey(TYPE, GROWL_SERVER_KEY);
+    private static final PropertyKey PASSWORD_KEY = new NotificatorPropertyKey(TYPE, GROWL_PASSWORD_KEY);
+        
     /** 
      * Map to hold info each user than has added this notifier to Growl.
      */
@@ -52,17 +54,11 @@ public class GrowlNotifier implements Notificator {
      * 
      */
     public GrowlNotifier(NotificatorRegistry nr) {
-        LOG.info("Registering GrowlNotifier...");
+        LOG.debug("Registering GrowlNotifier...");
         userProps = new ArrayList<UserPropertyInfo>();
         userProps.add(new UserPropertyInfo(GROWL_SERVER_KEY, "Growl Server IP"));
         userProps.add(new UserPropertyInfo(GROWL_PASSWORD_KEY, "Growl Server Password"));
         nr.register(this, userProps);
-        try {
-            growl = new Growl();
-        } catch(GrowlException e) {
-            LOG.error("Error creating Growl instance...", e);
-        }
-        
     }
     
     public String getDisplayName() {
@@ -70,117 +66,113 @@ public class GrowlNotifier implements Notificator {
     }
     
     public String getNotificatorType() {
-        return "tcGrowlNotifier";
+        return TYPE;
     }
 	
 
 	/* (non-Javadoc)
 	 * @see jetbrains.buildServer.notification.Notificator#notifyBuildFailed(jetbrains.buildServer.serverSide.SRunningBuild, java.util.Set)
 	 */
-	public void notifyBuildFailed(SRunningBuild srb, Set<SUser> users) {
-		// TODO Auto-generated method stub
-		LOG.info("notifyBuildFailed");
-		doNotifications("Build " + srb.getFullName() + " failed " + srb.getBuildNumber(), users);
+	public void notifyBuildFailed(SRunningBuild srb, Set<SUser> users) {		
+		LOG.debug("notifyBuildFailed");
+		doNotifications("Build " + srb.getFullName() + " failed " + 
+		                srb.getBuildNumber(), users, GrowlNotification.HIGH);
 	}
 
 	/* (non-Javadoc)
 	 * @see jetbrains.buildServer.notification.Notificator#notifyBuildFailing(jetbrains.buildServer.serverSide.SRunningBuild, java.util.Set)
 	 */
-	public void notifyBuildFailing(SRunningBuild srb, Set<SUser> users) {
-		// TODO Auto-generated method stub
-	    LOG.info("notifyBuildFailing");
-	    doNotifications("Build " + srb.getFullName() + " failing " + srb.getBuildNumber(), users);
+	public void notifyBuildFailing(SRunningBuild srb, Set<SUser> users) {		
+	    LOG.debug("notifyBuildFailing");
+	    doNotifications("Build " + srb.getFullName() + " failing " + srb.getBuildNumber(), users, GrowlNotification.HIGH);
 	}
 
 	/* (non-Javadoc)
 	 * @see jetbrains.buildServer.notification.Notificator#notifyBuildProbablyHanging(jetbrains.buildServer.serverSide.SRunningBuild, java.util.Set)
 	 */
-	public void notifyBuildProbablyHanging(SRunningBuild srb, Set<SUser> users) {
-		// TODO Auto-generated method stub
-	    doNotifications("Build " + srb.getFullName() + " probably hanging " + srb.getBuildNumber(), users);
+	public void notifyBuildProbablyHanging(SRunningBuild srb, Set<SUser> users) {	
+	    LOG.debug("notifyBuildProbablyHanging");	
+	    doNotifications("Build " + srb.getFullName() + " probably hanging " + srb.getBuildNumber(), users, GrowlNotification.MODERATE);
 	}
 
 	/* (non-Javadoc)
 	 * @see jetbrains.buildServer.notification.Notificator#notifyBuildStarted(jetbrains.buildServer.serverSide.SRunningBuild, java.util.Set)
 	 */
-	public void notifyBuildStarted(SRunningBuild srb, Set<SUser> users) {
-		// TODO Auto-generated method stub
-        doNotifications("Build " + srb.getFullName() + " started " + srb.getBuildNumber(), users);
+	public void notifyBuildStarted(SRunningBuild srb, Set<SUser> users) {		
+	    LOG.debug("notifyBuildStarted");
+        doNotifications("Build " + srb.getFullName() + " started " + srb.getBuildNumber(), users, GrowlNotification.NORMAL);
 	}
 
 	/* (non-Javadoc)
 	 * @see jetbrains.buildServer.notification.Notificator#notifyBuildSuccessful(jetbrains.buildServer.serverSide.SRunningBuild, java.util.Set)
 	 */
-	public void notifyBuildSuccessful(SRunningBuild srb, Set<SUser> users) {
-		// TODO Auto-generated method stub
-	    LOG.info("notifyBuildSuccessful");
-	    doNotifications("Build " + srb.getFullName() + " successful " + srb.getBuildNumber(), users);
+	public void notifyBuildSuccessful(SRunningBuild srb, Set<SUser> users) {		
+	    LOG.debug("notifyBuildSuccessful");
+	    doNotifications("Build " + srb.getFullName() + " successful " + srb.getBuildNumber(), users, GrowlNotification.NORMAL);
 	}
 
 	/* (non-Javadoc)
 	 * @see jetbrains.buildServer.notification.Notificator#notifyResponsibleChanged(jetbrains.buildServer.serverSide.SBuildType, java.util.Set)
 	 */
-	public void notifyResponsibleChanged(SBuildType sbt, Set<SUser> users) {
-		// TODO Auto-generated method stub
-	    LOG.info("notifyResponsibleChanged");
-	    doNotifications("Responsible user changed...", users);
+	public void notifyResponsibleChanged(SBuildType sbt, Set<SUser> users) {		
+	    LOG.debug("notifyResponsibleChanged");
+	    doNotifications("Responsible user changed...", users, GrowlNotification.NORMAL);
 	}
 	
+	/* (non-Javadoc)
+	 * @see jetbrains.buildServer.notification.Notificator#notifyLabelingFailed(jetbrains.buildServer.Build, jetbrains.buildServer.vcs.VcsRoot, java.lang.Throwable, java.util.Set)
+	 */
 	public void notifyLabelingFailed(jetbrains.buildServer.Build build,
 	        jetbrains.buildServer.vcs.VcsRoot root,
 	        java.lang.Throwable t,
 	        java.util.Set<jetbrains.buildServer.users.SUser> users) {
-	    LOG.info("notifyLabelingFailed...");
-	    doNotifications("Labeling failed...", users);
+	    LOG.debug("notifyLabelingFailed");
+	    doNotifications("Labeling failed...", users, GrowlNotification.HIGH);
 	}
 	
 	/**
 	 * Send the growl notifications to the specified users.
+	 * 
+	 * @param message The message to send
 	 * @param users The users to notify
+	 * @param notificationLevel Priority of the notification
+	 *
 	 */
-	private void doNotifications(String message, Set<SUser> users) {
+	private void doNotifications(String message, Set<SUser> users, int notificationLevel) {
+
 	    
-	    for(SUser user : users) {
-	        String[] info = getGrowlInfo(user);	        
-	        if(info[0] != null) {	            	        
-	            growl.addGrowlHost(info[0], info[1]);
-                GrowlRegistrations registrations = growl.getRegistrations(APP_NAME);
+	    for(SUser user : users) {	        
+            LOG.debug("notifying user: " + user.getUsername());
+            String username = user.getUsername();
+            String growlServer = user.getPropertyValue(SERVER_KEY);
+            String growlPasswd = user.getPropertyValue(PASSWORD_KEY);
+                        
+            if(growlServer != null && growlServer.length() != 0) {
+                LOG.debug("Sending message '" + message + "' to " + growlServer);
+            try {
+
+                // TODO: Cache Growl instance per user
+                // Need to figure out how to tell if the user's Growl settings have changed
+                // requiring the cache to be updated
+                Growl g = new Growl();
+                g.addGrowlHost(growlServer, growlPasswd);
+                GrowlRegistrations registrations = g.getRegistrations(APP_NAME);
                 registrations.registerNotification(APP_NAME, true);
-                try {
-                    growl.sendNotification(new GrowlNotification(APP_NAME, 
-                                                                APP_NAME, 
-                                                                message, 
-                                                                APP_NAME, 
-                                                                false, 
-                                                                GrowlNotification.NORMAL));                                
+
+                g.sendNotification(new GrowlNotification(APP_NAME, 
+                                                         APP_NAME, 
+                                                         message, 
+                                                         APP_NAME, 
+                                                         false, 
+                                                         notificationLevel));                                
                 } catch(GrowlException e) {
                     LOG.error("Error sending growl notification", e);
                     e.printStackTrace();
                 }               
-            }
+            } 
             
 	    }    
 	    
 	}
 	
-	/**
-	 * Get the user's settings.
-	 * @param user The user
-	 * @return String array containing growl server and growl password
-	 */
-    private String[] getGrowlInfo(SUser user) {        
-        String[] info = new String[2];
-        //plugin:notificator:tcGrowlNotifier:gServer === localhost
-        //plugin:notificator:tcGrowlNotifier:tcgrowl.gPassword === 
-        
-        Map<PropertyKey, String> p = user.getProperties();
-        for(PropertyKey pk : p.keySet()) {
-            if(pk.getKey().equals(PROP_ROOT+GROWL_SERVER_KEY)) {
-                info[0] = p.get(pk);
-            } else if(pk.getKey().equals(PROP_ROOT+GROWL_PASSWORD_KEY)) {
-                info[1] = p.get(pk);
-            }            
-        }
-        return info;
-    }
 }
